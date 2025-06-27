359d81cbea110d5320a1f9da195d2b81e5ed984c
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 16:16:44 +0900

    間違ってprofile上に作成したコードを削除

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import EmployeeProfileCreateView, EmployeeProfileDetailView, EmployeeProfileUpdateView,sign_up
+from .views import EmployeeProfileCreateView, EmployeeProfileDetailView, EmployeeProfileUpdateView
 
 urlpatterns = [
     path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
     path('detail/', EmployeeProfileDetailView.as_view(), name='profile_detail'),
     path('edit/', EmployeeProfileUpdateView.as_view(), name='profile_edit'),
-    path('sign_up/', sign_up, name='sign_up'),
 ]
