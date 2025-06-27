42e8a3e81b53b5f0b4d857b7d11e715a6d9893ce
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-27 15:54:47 +0900

    写真追加のためにモデルを変更しました

--- profiles/models.py
+++ profiles/models.py
@@ -6,6 +6,7 @@ class EmployeeProfile(models.Model):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True) 
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
