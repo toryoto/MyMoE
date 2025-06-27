a9e957ba5cced89aede3e9b98416d17ebeea9315
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 17:08:24 +0900

    signupformをインポートし、追加しました

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import EmployeeCreationForm
+from .forms import SignUpForm
 
 def mymoe_home(request):
     if not request.user.is_authenticated:
@@ -9,11 +10,11 @@ def mymoe_home(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = EmployeeCreationForm(request.POST)
+        form = SignUpForm(request.POST) 
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('mymoe_home')
     else:
-        form = EmployeeCreationForm()
+        form = SignUpForm()
     return render(request, 'employees/signup.html', {'form': form})
