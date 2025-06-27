68e2c20c26ec0c906a97f8fac9d99e818d49208a
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 14:59:08 +0900

    フォームにバリデーションを追加

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import EmployeeProfile
+from django.core.validators import RegexValidator
 
 class EmployeeProfileForm(forms.ModelForm):
     class Meta:
@@ -8,3 +9,17 @@ class EmployeeProfileForm(forms.ModelForm):
         widgets = {
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
+
+from django import forms
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(
+        label='ユーザーID',
+        max_length=50,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z]+\.[a-zA-Z]+$',
+                message='EIDのみ入力可能です。'
+            )
+        ]
+    )
