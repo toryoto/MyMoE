dda94acc8bac679f991bfc4d0790af88337d415a
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 14:58:20 +0900

    profileアプリのurlsにパスを設定

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EmployeeProfileCreateView, EmployeeProfileDetailView, EmployeeProfileUpdateView
+from .views import EmployeeProfileCreateView, EmployeeProfileDetailView, EmployeeProfileUpdateView,sign_up
 
 urlpatterns = [
     path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
     path('detail/', EmployeeProfileDetailView.as_view(), name='profile_detail'),
     path('edit/', EmployeeProfileUpdateView.as_view(), name='profile_edit'),
+    path('sign_up/', sign_up, name='sign_up'),
 ]
