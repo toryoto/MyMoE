49f3419f038f02123919039d14a0e7edeac5257e
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-27 16:13:54 +0900

    開発モードの時だけ、mediaフォルダのファイルをURLから直性アクセスできるように、url.pyを変更

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('detail/', EmployeeProfileDetailView.as_view(), name='profile_detail'),
     path('edit/', EmployeeProfileUpdateView.as_view(), name='profile_edit'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.conf import settings
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
