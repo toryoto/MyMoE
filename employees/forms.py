532f39add87bc3b5c5ee61f12014d8ccc9c79fd1
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 16:18:38 +0900

    employee下にバリデーション機能を実装

--- employees/forms.py
+++ employees/forms.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import Employee
+from django.core.validators import RegexValidator
 
 class EmployeeCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Employee
         fields = ('username', 'email')
+
+from django import forms
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(
+        label='ユーザーID',
+        max_length=50,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z]+\.[a-zA-Z]+$',
+                message='EIDのみ入力可能です。'
+            )
+        ]
+    )
