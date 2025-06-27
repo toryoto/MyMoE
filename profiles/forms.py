6452d4de3a6abd5bb6e9fcd01609811af154aacc
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-27 16:17:51 +0900

    間違ってprofileに作成したコードを削除

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import EmployeeProfile
-from django.core.validators import RegexValidator
 
 class EmployeeProfileForm(forms.ModelForm):
     class Meta:
@@ -10,16 +9,4 @@ class EmployeeProfileForm(forms.ModelForm):
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
 
-from django import forms
 
-class SignUpForm(forms.Form):
-    username = forms.CharField(
-        label='ユーザーID',
-        max_length=50,
-        validators=[
-            RegexValidator(
-                regex=r'^[a-zA-Z]+\.[a-zA-Z]+$',
-                message='EIDのみ入力可能です。'
-            )
-        ]
-    )
