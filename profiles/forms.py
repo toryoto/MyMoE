9e471fae1c0668c20503bc3ff1021fbd0dda4ede
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-27 16:15:56 +0900

    froms.pyに写真部分を追加

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,7 +4,7 @@ from .models import EmployeeProfile
 class EmployeeProfileForm(forms.ModelForm):
     class Meta:
         model = EmployeeProfile
-        fields = ['phone_number', 'date_of_birth', 'bio']
+        fields = ['phone_number', 'date_of_birth', 'bio', 'photo']
         widgets = {
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
