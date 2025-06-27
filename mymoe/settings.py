6537706a1f09f2a798ca2c1e8e7c68b5bff131fb
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-27 16:07:30 +0900

    setting.pyにメディア設定を追加しました

--- mymoe/settings.py
+++ mymoe/settings.py
@@ -128,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'employees.Employee'
 LOGIN_REDIRECT_URL = '/mymoe/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+import os 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Ensure that the media directory exists
