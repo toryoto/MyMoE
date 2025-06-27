e7decec60bd868740847ffb9cb764977139ea15c
shima.endo <shima.endo@accenture.com>
2025-06-27 16:09:54 +0900

    viers.pyの不要なclassを削除しました

--- departments/views.py
+++ departments/views.py
@@ -47,36 +47,3 @@ class DepartmentDeleteView(SuperuserRequiredMixin, DeleteView):
     model = Department
     template_name = 'departments/department_confirm_delete.html'
     success_url = reverse_lazy('departments:department_list')
-
-
-class DTEListView(LoginRequiredMixin, ListView):
-    model = DTE
-    template_name = 'departments/dte_list.html'
-    context_object_name = 'dtes'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['employee'] = self.request.user
-        return context
-
-class DTEDetailView(LoginRequiredMixin, DetailView):
-    model = DTE
-    template_name = 'departments/dte_detail.html'
-    context_object_name = 'dte'
-
-class DTECreateView(SuperuserRequiredMixin, CreateView):
-    model = DTE
-    template_name = 'departments/dte_form.html'
-    fields = ['name', 'code', 'department', 'description', 'is_active']
-    success_url = reverse_lazy('departments:dte_list')
-
-class DTEUpdateView(SuperuserRequiredMixin, UpdateView):
-    model = DTE
-    template_name = 'departments/dte_form.html'
-    fields = ['name', 'code', 'department', 'description', 'is_active']
-    success_url = reverse_lazy('departments:dte_list')
-
-class DTEDeleteView(SuperuserRequiredMixin, DeleteView):
-    model = DTE
-    template_name = 'departments/dte_confirm_delete.html'
-    success_url = reverse_lazy('departments:dte_list')
