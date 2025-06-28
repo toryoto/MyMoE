ead05d99089fd116ebd91c535bbf481051db14f0
ryoto.taga <ryoto.taga@accenture.com>
2025-06-29 03:27:53 +0900

    only hr change employees' department and dte

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import EmployeeProfileCreateView, MyProfileDetailView, EmployeeProfileDetailView, EmployeeProfileUpdateView
+from .views import (
+    EmployeeProfileCreateView, MyProfileDetailView, EmployeeProfileUpdateView, EmployeeProfileDetailView, UpdateEmployeeOrgInfoView, GetDTEsForDepartmentView
+)
+
+app_name = 'profiles'
 
 urlpatterns = [
+    path('my-profile/', MyProfileDetailView.as_view(), name='my_profile_detail'),
+    path('my-profile/update/', EmployeeProfileUpdateView.as_view(), name='my_profile_update'),
     path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
-    path('my_profile/', MyProfileDetailView.as_view(), name='my_profile_detail'), # ログインユーザーのプロフィール詳細
-    path('detail/<int:pk>/', EmployeeProfileDetailView.as_view(), name='profile_detail'), # 他の社員のプロフィール詳細
-    path('edit/', EmployeeProfileUpdateView.as_view(), name='profile_edit'),
+    path('<int:pk>/', EmployeeProfileDetailView.as_view(), name='employee_profile_detail'),
+    path('update-org-info/<int:pk>/', UpdateEmployeeOrgInfoView.as_view(), name='update_org_info'),
+    path('get-dtes/<int:department_id>/', GetDTEsForDepartmentView.as_view(), name='get_dtes_for_department'),
 ]
