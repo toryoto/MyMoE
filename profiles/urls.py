5b2e08c7b721f93e5ecd56afc8eadc18d5cd07f5
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-30 10:49:14 +0900

    衝突解消、両方取り込み

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import EmployeeProfileCreateView, EmployeeProfileDetailView, EmployeeProfileUpdateView
 from django.conf import settings
 
+from .views import (
+    EmployeeProfileCreateView, MyProfileDetailView, EmployeeProfileUpdateView, EmployeeProfileDetailView, 
+    UpdateEmployeeOrgInfoView, GetDTEsForDepartmentView, SkillSearchView
+)
+
+app_name = 'profiles'
 
 urlpatterns = [
+    path('my-profile/', MyProfileDetailView.as_view(), name='my_profile_detail'),
+    path('my-profile/update/', EmployeeProfileUpdateView.as_view(), name='my_profile_update'),
     path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
-    path('detail/', EmployeeProfileDetailView.as_view(), name='profile_detail'),
-    path('edit/', EmployeeProfileUpdateView.as_view(), name='profile_edit'),
+    path('<int:pk>/', EmployeeProfileDetailView.as_view(), name='employee_profile_detail'),
+    path('update-org-info/<int:pk>/', UpdateEmployeeOrgInfoView.as_view(), name='update_org_info'),
+    path('get-dtes/<int:department_id>/', GetDTEsForDepartmentView.as_view(), name='get_dtes_for_department'),
+    path('api/skills/search/', SkillSearchView.as_view(), name='skill_search'),
 ]
 
 if settings.DEBUG:
