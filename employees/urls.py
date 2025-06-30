824b9283412ff1f20ced017e53192a5b04300b4a
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-30 10:38:22 +0900

    衝突なし

--- employees/urls.py
+++ employees/urls.py
@@ -3,7 +3,14 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='employees/login.html'), name='login'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('force-password-change/',views.ForcePasswordChangeView.as_view(), name='force_password_change'),
+    # 社員一覧
+    path('list/', views.EmployeeListView.as_view(), name='employee_list'),
+    # CSV登録関連
+    path('csv-import/', views.CSVBulkImportView.as_view(), name='csv_bulk_import'),
+    path('csv-preview/', views.CSVPreviewView.as_view(), name='csv_preview'),
+    path('csv-result/', views.CSVResultView.as_view(), name='csv_result'),
 ]
