13d368cf257fb6c3bfe2b5026d595ac2699cf54a
natsuki.rokkaku <natsuki.rokkaku@accenture.com>
2025-06-30 10:44:12 +0900

    衝突解消、両方を取り込む

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.conf import settings
 
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class EmployeeProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True) 
+    skills = models.ManyToManyField(Skill, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
