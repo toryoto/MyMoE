cad715a489b6d9744a43e0de52b4e2cb26522d45
shima.endo <shima.endo@accenture.com>
2025-06-30 14:22:27 +0900

    MLの入力規則変更

--- profiles/models.py
+++ profiles/models.py
@@ -14,7 +14,20 @@ class EmployeeProfile(models.Model):
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     dte = models.CharField(max_length=100)
-    ml = models.IntegerField()
+    ML_CHOICES = [
+        ('1','SMD'),
+        ('2-4','Managing Director'),
+        ('5','Associate Director'),
+        ('6','Senior Manager'),
+        ('7','Manager'),
+        ('8','Associate Manager'),
+        ('9','Team Lead/Consultant'),
+        ('10','Sonior Analyst'),
+        ('11','Analyst'),
+        ('12','Associate'),
+        ('13','New Associate')
+    ]
+    ml = models.CharField(max_length=50,choices= ML_CHOICES ,default='11')
     mail_address = models.CharField(max_length=100)
     date_of_birth = models.DateField()
     statement_id = models.CharField(max_length=10)
