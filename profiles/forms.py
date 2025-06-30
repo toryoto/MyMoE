b856bc8e824383aedf697e89353b5705acece006
hikaru.kohri <hikaru.kohri@accenture.com>
2025-06-30 15:20:08 +0900

    電話番号のバリデーションを追加しました

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import EmployeeProfile, Skill
 from django.forms import ClearableFileInput
+import re
 
 class EmployeeProfileForm(forms.ModelForm):
     skills = forms.ModelMultipleChoiceField(
@@ -19,3 +20,13 @@ class EmployeeProfileForm(forms.ModelForm):
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
             'photo': ClearableFileInput(),
         }
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+
+        # ハイフンを含んでいるかどうかをチェック
+        pattern = r'^\d{2,4}-\d{2,4}-\d{3,4}$'
+        if not re.match(pattern, phone_number):
+            raise forms.ValidationError("電話番号は「090-1234-5678」の形式で入力してください")
+
+        return phone_number
