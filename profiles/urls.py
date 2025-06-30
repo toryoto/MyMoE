651c8e4929ecde9233b33238813e939811b35470
ryoto.taga <ryoto.taga@accenture.com>
2025-06-30 17:52:49 +0900

    pre employment

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,8 +3,7 @@ from django.conf import settings
 
 from .views import (
     EmployeeProfileCreateView, MyProfileDetailView, EmployeeProfileUpdateView, EmployeeProfileDetailView, 
-    UpdateEmployeeOrgInfoView, GetDTEsForDepartmentView, SkillSearchView, PreEmploymentHistoryCreateView,
-    PreEmploymentHistoryUpdateView, 
+    SkillSearchView,
 )
 
 app_name = 'profiles'
@@ -14,11 +13,7 @@ urlpatterns = [
     path('my-profile/update/', EmployeeProfileUpdateView.as_view(), name='my_profile_update'),
     path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
     path('<int:pk>/', EmployeeProfileDetailView.as_view(), name='employee_profile_detail'),
-    path('update-org-info/<int:pk>/', UpdateEmployeeOrgInfoView.as_view(), name='update_org_info'),
-    path('get-dtes/<int:department_id>/', GetDTEsForDepartmentView.as_view(), name='get_dtes_for_department'),
     path('api/skills/search/', SkillSearchView.as_view(), name='skill_search'),
-    path('pre-employment/add/', PreEmploymentHistoryCreateView.as_view(), name='pre_employment_add'),
-    path('pre-employment/<int:pk>/edit/', PreEmploymentHistoryUpdateView.as_view(), name='pre_employment_edit'),
 ]
 
 if settings.DEBUG:
--- profiles/views.py
+++ profiles/views.py
@@ -27,12 +27,23 @@ class EmployeeProfileCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['history_formset'] = PreEmploymentHistoryFormSet(self.request.POST, self.request.FILES, prefix='history')
+        else:
+            context['history_formset'] = PreEmploymentHistoryFormSet(prefix='history')
         context['initial_skill_ids'] = []
         return context
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super().form_valid(form)
+        self.object = form.save()
+        history_formset = PreEmploymentHistoryFormSet(self.request.POST, self.request.FILES, instance=self.object, prefix='history')
+        if history_formset.is_valid():
+            history_formset.instance = self.object
+            history_formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
 
 class MyProfileDetailView(LoginRequiredMixin, DetailView):
     model = EmployeeProfile
@@ -69,128 +80,28 @@ class EmployeeProfileUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateV
     template_name = 'profiles/employee_profile_form.html'
     success_url = reverse_lazy('profiles:my_profile_detail')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # テンプレートに渡すために、現在のスキルIDのリストを作成
-        context['initial_skill_ids'] = list(self.object.skills.values_list('id', flat=True))
-        context['history_formset'] = PreEmploymentHistoryFormSet(instance=self.object)
-
-        return context
-
-    def dispatch(self, request, *args, **kwargs):
-        # プロフィールが存在しない場合は作成(employees/signals.pyで自動作成されているが、念の為)
-        profile, created = EmployeeProfile.objects.get_or_create(user=request.user)
-        if created:
-            # 新規作成の場合は、オブジェクトをセットしてからUpdateViewの処理を継続
-            self.kwargs['pk'] = profile.pk
-        
-        return super().dispatch(request, *args, **kwargs)
-
     def get_object(self, queryset=None):
-        # URLのpkではなく、常にログインユーザーのプロフィールを返す
         return get_object_or_404(EmployeeProfile, user=self.request.user)
-
-    def test_func(self):
-        profile = self.get_object()
-        return self.request.user == profile.user
-
-class UpdateEmployeeOrgInfoView(LoginRequiredMixin, View):
-    def post(self, request, pk):
-        if not request.user.is_hr:
-            return JsonResponse({'status': 'error', 'message': 'Permission denied.'}, status=403)
-
-        try:
-            employee = get_object_or_404(Employee, pk=pk)
-            data = json.loads(request.body)
-            department_id = data.get('department_id')
-            dte_id = data.get('dte_id')
-
-            department = get_object_or_404(Department, id=department_id) if department_id else None
-            dte = get_object_or_404(DTE, id=dte_id) if dte_id else None
-
-            employee.department = department
-            employee.dte = dte
-            employee.clean()
-            employee.save()
-
-            html = render_to_string('components/user_org_info.html', {'employee': employee})
-
-            return JsonResponse({'status': 'success', 'message': 'Organization info updated.', 'html': html})
-        except Exception as e:
-            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
-
-class GetDTEsForDepartmentView(LoginRequiredMixin, View):
-    def get(self, request, department_id):
-        dtes = DTE.objects.filter(department_id=department_id, is_active=True).values('id', 'name')
-        return JsonResponse(list(dtes), safe=False)
-
-
-class PreEmploymentHistoryCreateView(LoginRequiredMixin, CreateView):
-    model = PreEmploymentHistory
-    form_class = PreEmploymentHistoryForm
-    template_name = 'profiles/pre_employment_history_form.html'
-    success_url = reverse_lazy('profiles:my_profile_detail')
-
-    def form_valid(self, form):
-        form.instance.employee = get_object_or_404(EmployeeProfile, user=self.request.user)
-        return super().form_valid(form)
-    
-class PreEmploymentHistoryUpdateView(LoginRequiredMixin, UpdateView):
-    model = PreEmploymentHistory
-    form_class = PreEmploymentHistoryForm
-    template_name = 'profiles/pre_employment_history_form.html'
-    success_url = reverse_lazy('profiles:my_profile_detail')
-
-    def get_queryset(self):
-        # 自分の経歴のみ編集可能にする
-        return PreEmploymentHistory.objects.filter(employee__user=self.request.user)
-
-class EmployeeProfileDeleteView(LoginRequiredMixin, DetailView):
-    model = EmployeeProfile
-    template_name = 'profiles/employee_profile_detail.html'
-    context_object_name = 'profile'
-
-    def get_object(self, queryset=None):
-        profile = super().get_object(queryset)
-
-        if profile.user == self.request.user:
-            return profile
-        if self.request.user.is_hr:
-            return profile
-
-        raise HttpResponseForbidden("You do not have permission to delete this profile.")
-
-from .forms import EmployeeProfileForm, PreEmploymentHistoryFormSet
-
-class EmployeeProfileUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = EmployeeProfile
-    form_class = EmployeeProfileForm
-    template_name = 'profiles/employee_profile_form.html'
-    success_url = reverse_lazy('profiles:my_profile_detail')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.POST:
-            context['history_formset'] = PreEmploymentHistoryFormSet(self.request.POST, instance=self.object)
+            context['history_formset'] = PreEmploymentHistoryFormSet(self.request.POST, self.request.FILES, instance=self.object, prefix='history')
         else:
-            context['history_formset'] = PreEmploymentHistoryFormSet(instance=self.object)
+            context['history_formset'] = PreEmploymentHistoryFormSet(instance=self.object, prefix='history')
 
         context['initial_skill_ids'] = list(self.object.skills.values_list('id', flat=True))
         return context
 
     def form_valid(self, form):
-        history_formset = PreEmploymentHistoryFormSet(self.request.POST, instance=self.object)
-        if history_formset.is_valid():
+        history_formset = PreEmploymentHistoryFormSet(self.request.POST, self.request.FILES, instance=self.object, prefix='history')
+        if form.is_valid() and history_formset.is_valid():
             self.object = form.save()
             history_formset.instance = self.object
             history_formset.save()
-            return super().form_valid(form)
+            return redirect(self.get_success_url())
         else:
-            return self.render_to_response(self.get_context_data(form=form, history_formset=history_formset))
-
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(EmployeeProfile, user=self.request.user)
+            return self.render_to_response(self.get_context_data(form=form))
 
     def test_func(self):
         profile = self.get_object()
