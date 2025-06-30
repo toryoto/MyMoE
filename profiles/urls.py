996399b3a7140349975559cb6252f1d57ef2fc4b
ryoto.taga <ryoto.taga@accenture.com>
2025-07-01 02:23:09 +0900

    merge

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,21 +1,23 @@
 from django.urls import path
 from django.conf import settings
 
-from .views import (
-    EmployeeProfileCreateView, MyProfileDetailView, EmployeeProfileUpdateView, EmployeeProfileDetailView, 
-    UpdateEmployeeOrgInfoView, GetDTEsForDepartmentView, SkillSearchView
+from profiles.views import (
+    EmployeeProfileUpdateView, EmployeeProfileDetailView, 
+    SkillSearchView, UpdateEmployeeOrgInfoView, EmployeeProfilePDFView
 )
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('my-profile/', MyProfileDetailView.as_view(), name='my_profile_detail'),
-    path('my-profile/update/', EmployeeProfileUpdateView.as_view(), name='my_profile_update'),
-    path('create/', EmployeeProfileCreateView.as_view(), name='profile_create'),
     path('<int:pk>/', EmployeeProfileDetailView.as_view(), name='employee_profile_detail'),
-    path('update-org-info/<int:pk>/', UpdateEmployeeOrgInfoView.as_view(), name='update_org_info'),
-    path('get-dtes/<int:department_id>/', GetDTEsForDepartmentView.as_view(), name='get_dtes_for_department'),
+    path('<int:pk>/edit/', EmployeeProfileUpdateView.as_view(), name='employee_profile_update'),
+    
+    # API エンドポイント
     path('api/skills/search/', SkillSearchView.as_view(), name='skill_search'),
+    path('update-org-info/<int:employee_id>/', UpdateEmployeeOrgInfoView.as_view(), name='update_employee_org_info'),
+    
+    # PDF Download
+    path('<int:pk>/pdf/', EmployeeProfilePDFView.as_view(), name='profile_pdf'), 
 ]
 
 if settings.DEBUG:
