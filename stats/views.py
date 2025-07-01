f1e03c7a9f4098e9d36095e3f227b304d1fbfa55
hikaru.kohri <hikaru.kohri@accenture.com>
2025-07-01 15:44:41 +0900

    社員数を表す（新卒/中途）棒グラフを追加しました

--- stats/views.py
+++ stats/views.py
@@ -3,6 +3,10 @@ from employees.models import Employee
 from departments.models import Department
 import json
 from collections import Counter
+from profiles.models import EmployeeProfile, PreEmploymentHistory
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.db.models import Count
 
 def department_pie_chart(request):
     # NullでないDepartmentに属する社員を集計
@@ -12,8 +16,16 @@ def department_pie_chart(request):
     labels = list(department_counts.keys())
     data = list(department_counts.values())
 
+    profiles = EmployeeProfile.objects.annotate(pre_count=Count('pre_employment_histories'))
+    mid_career_count = profiles.filter(pre_count__gt=0).count()
+    new_graduate_count = profiles.filter(pre_count=0).count()
+    bar_labels = ['新卒', '中途']
+    bar_data = [new_graduate_count, mid_career_count]
+
     context = {
         'labels': json.dumps(labels, ensure_ascii=False),
         'data': data,
+        'bar_labels': json.dumps(bar_labels, ensure_ascii=False),
+        'bar_data': bar_data,
     }
     return render(request, 'stats/department_pie_chart.html', context)
